108ac7f7858b37e7e5eac426f0a0a61ea6ecda35
EmmanuelWambua <emmanuelwambua931@gmail.com>
2023-03-17 20:47:35 +0300

    done with lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const people = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(people, eventName){
+    let thankYouMesseges = []
+    for(let i = 0; i <  people.length; i++){
+        thankYouMesseges.push(`Thank you, ${people[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return  thankYouMesseges;
+}
+
+function countDown(number){
+    let num = number
+    while(num >= 0){
+        console.log(num--)
+    }
+}
+
+
